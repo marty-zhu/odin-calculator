8e3950ee0dd8c04885d102708ecb8e415fd804ec
marty-zhu <marty.zhu@gmail.com>
2022-10-24 19:47:15 -0400

    added basic numpad key event listener to js/main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-const calKeys = document.querySelectorAll(".cal-key");
+// separate number keys and operations keys
+const numpadKeys = document.querySelectorAll(".cal-numkey");
+const opsKeys = document.querySelectorAll(".cal-opskey");
+// isolate the clear key and equals key
+const enterKey = document.querySelector("div#cal-exec");
+const clearKey = document.querySelector("div#cal-clear");
+
 const calViewport = document.querySelector("div#cal-viewport");
 
-calKeys.forEach((calKey) => {
-    calKey.addEventListener('click', (e) => {
-        calViewport.textContent = e.target.textContent;
-    });
+// create variable to store numbers, one digit at a time
+let numEntry = new Array();
+// when an ops key is pressed, convert the stored numbers to one numerical variable, conclude previous calculations
+// display the entered number or calculation result
+// if the "=" key is pressed, conclude calculations and display result
+// if the "CLEAR" key is pressed, clear all variables
+
+numpadKeys.forEach((numKey) => {
+    numKey.addEventListener('click', (e) => {
+        numEntry.push(e.target.textContent);
+        console.log(numEntry);
+    })
 })

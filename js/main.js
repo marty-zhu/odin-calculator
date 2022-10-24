835231e169d2de326b86a8b167de64c04a3c0991
marty-zhu <marty.zhu@gmail.com>
2022-10-24 18:47:13 -0400

    updated event listener to update calculator viewport

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const calKeys = document.querySelectorAll(".cal-key");
+const calViewport = document.querySelector("div#cal-viewport");
 
 calKeys.forEach((calKey) => {
-    calKey.addEventListener('click', (e) => console.log(`${e.target.textContent}`));
+    calKey.addEventListener('click', (e) => {
+        calViewport.textContent = e.target.textContent;
+    });
 })

654c26f867c44a02236e1a586e8f174ffb94e566
marty-zhu <marty.zhu@gmail.com>
2022-11-27 10:03:03 -0800

    updated TODO list at top of js file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 // TODO: 
-//  - make updateViewport where the rounding happens - DONE!
-//  - make updateNumEntered limite integer digits to 7 - DONE!
-//  - add rounding to calculation results - DONE!
-//  - keep ops keys highlighted until CLEAR, replacement by another ops key, or RETURN
+// - prettify app
 
 // 0. DOM OBJECTS
 
